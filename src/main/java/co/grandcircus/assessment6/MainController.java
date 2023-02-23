6addf91028e75ee4354fad36388eb80e60d1e156
David Wolverton <dwolvert@gmail.com>
2023-02-22 22:11:40 -0500

    finished version from class

--- src/main/java/co/grandcircus/assessment6/MainController.java
+++ src/main/java/co/grandcircus/assessment6/MainController.java
@@ -1,13 +1,26 @@
 package co.grandcircus.assessment6;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import co.grandcircus.assessment6.model.Plant;
+import co.grandcircus.assessment6.repository.PlantRepository;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	PlantRepository plantRepo;
 
 	@RequestMapping("/")
-	public String homepage() {
+	public String homepage(Model model) {
+		List<Plant> plants = plantRepo.findAll();
+		model.addAttribute("plants", plants);
+		
 		return "home";
 	}
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,5 +9,11 @@
 </head>
 <body>
 	<h1>Hello World</h1>
+	
+	<ul>
+		<c:forEach var="plant" items="${plants}">
+			<li>${ plant.name } - ${ plant.height }ft</li>
+		</c:forEach>
+	</ul>
 </body>
 </html>
